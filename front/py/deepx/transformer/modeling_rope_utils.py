79c2fde9f799f07bfb3e225a061db79160d76a27
lipeng <734991033@qq.com>
2025-05-08 17:26:51 +0800

    rope:实现_compute_default_rope_parameters

--- front/py/deepx/transformer/modeling_rope_utils.py
+++ front/py/deepx/transformer/modeling_rope_utils.py
@@ -1,19 +1,21 @@
 from typing import   Tuple
 import math
-from deepx import arange,Tensor
+from deepx import arange,Tensor,where
 
 def _compute_default_rope_parameters(config:dict={
-    "base":10000.0,
+    "rope_theta":10000.0,
     "head_dim":0,
     "partial_rotary_factor":1.0,
 }) -> Tuple[Tensor, float]:
-    dim   = config.head_dim* config.partial_rotary_factor
+    partial_rotary_factor = config.get("partial_rotary_factor", 1.0)
+    dim   = config["head_dim"]* partial_rotary_factor
     # 计算逆频率
-    inv_freq = 1.0 / (config.base ** (arange(0, dim, 2, dtype='float64')/ dim))
+    base=config["rope_theta"]
+    inv_freq = 1.0 / (base ** (arange(0, dim, 2, dtype='float64')/ dim))
     return inv_freq, 1.0
     
 def _compute_llama3_parameters(config:dict={
-    "base":10000.0,
+    "rope_theta":10000.0,
     "head_dim":0,
     "partial_rotary_factor":1.0,
     "factor":8,
@@ -24,6 +26,7 @@ def _compute_llama3_parameters(config:dict={
 }) -> Tuple[Tensor, float]:
     # Gets the default RoPE parameters
     inv_freq, attention_factor = _compute_default_rope_parameters(config)
+    #TODO
 
     low_freq_wavelen = config.old_context_len / config.low_freq_factor
     high_freq_wavelen = config.old_context_len / config.high_freq_factor
@@ -31,12 +34,12 @@ def _compute_llama3_parameters(config:dict={
     wavelen = 2 * math.pi / inv_freq
     # wavelen < high_freq_wavelen: do nothing
     # wavelen > low_freq_wavelen: divide by factor
-    inv_freq_llama = torch.where(wavelen > low_freq_wavelen, inv_freq / config.factor, inv_freq)
+    inv_freq_llama =  where(wavelen > low_freq_wavelen, inv_freq / config.factor, inv_freq)
     # otherwise: interpolate between the two, using a smooth factor
     smooth_factor = (config.old_context_len / wavelen - config.low_freq_factor) / (config.high_freq_factor - config.low_freq_factor)
     smoothed_inv_freq = (1 - smooth_factor) * inv_freq_llama / config.factor + smooth_factor * inv_freq_llama
     is_medium_freq = ~(wavelen < high_freq_wavelen) * ~(wavelen > low_freq_wavelen)
-    inv_freq_llama = torch.where(is_medium_freq, smoothed_inv_freq, inv_freq_llama)
+    inv_freq_llama =  where(is_medium_freq, smoothed_inv_freq, inv_freq_llama)
 
     return inv_freq_llama, attention_factor
  
