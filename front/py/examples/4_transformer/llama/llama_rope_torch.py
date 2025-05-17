3984a99fd46f7d2459027e22ebae52a62663ce89
lipeng <734991033@qq.com>
2025-05-17 22:34:20 +0800

    rope,embedding验证:

--- front/py/examples/4_transformer/llama/llama_rope_torch.py
+++ front/py/examples/4_transformer/llama/llama_rope_torch.py
@@ -52,23 +52,25 @@ class NetTorch(torch.nn.Module):
         self.embed_tokens = torch.nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
         from transformers.models.llama.modeling_llama import LlamaRotaryEmbedding
         self.rotary_emb = LlamaRotaryEmbedding(config=config)
-
+        print("rotary_emb.inv_freq")
+        print(self.rotary_emb.inv_freq)
     def forward(self, x):
         inputs_embeds = self.embed_tokens(x)
+        print(inputs_embeds)
         hidden_states = inputs_embeds
         # create position embeddings to be shared across the decoder layers
         position_ids = torch.arange(hidden_states.shape[1], device=hidden_states.device).unsqueeze(0)
         return self.rotary_emb(hidden_states, position_ids)
 
+if __name__ == "__main__":
+    torch_net = NetTorch(config)
+    save_torch(torch_net.embed_tokens.weight, dir + 'weight')
+    # 前向传播
+    torch_output = torch_net(torch_input)
+    torch_sin, torch_cos = torch_output
 
-torch_net = NetTorch(config)
-save_torch(torch_net.embed_tokens.weight, dir + 'weight')
-# 前向传播
-torch_output = torch_net(torch_input)
-torch_sin, torch_cos = torch_output
+    print("sin shape:", torch_sin.shape)
+    print("sin:", torch_sin)
 
-print("sin shape:", torch_sin.shape)
-print("sin:", torch_sin)
-
-print("cos shape:", torch_cos.shape)
-print("cos:", torch_cos)
+    print("cos shape:", torch_cos.shape)
+    print("cos:", torch_cos)
