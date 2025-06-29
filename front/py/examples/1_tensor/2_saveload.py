c02ba98f0d5129be18e30ee11db0cc7a862d07ed
lipeng <734991033@qq.com>
2025-06-29 17:01:47 +0800

    excuter-cuda:解决bf16,fp16的打印问题

--- front/py/examples/1_tensor/2_saveload.py
+++ front/py/examples/1_tensor/2_saveload.py
@@ -1,35 +1,20 @@
-from deepx.nn.functional import arange,save,load
+from deepx.nn.functional import arange,save,load,full
 
 dir = '/home/lipeng/model/deepx/tester/'
 
-def saveloadfloat32():
-    t1=arange(start=0,end=60 ,dtype='float32').reshape_((3,4,5))
+def saveload(dtype:str='float32'):
+    print()
 
+
+    t1=full((3,4,5),2,dtype)
+    # t1=arange(start=0,end=60 ,dtype=dtype)
+    # t1=t.reshape_((3,4,5))
+    t1.float().print()
     t1.print()
-    t1.save(dir+'t1')
-
-    t2=load(dir+'t1')
+    name='t_'+dtype
+    t1.save(dir+name)
+    t2=load(dir+name)
     t2.print()
-
-def saveloadint8():
-    t=arange(start=0,end=60 ,dtype='int8').reshape_((3,4,5))
-
-    t.save(dir+'tint8')
-
-    t2=load(dir+"tint8")
-    t2.print()
-
-
-def saveloadbfloat16():
-    t=arange(start=0,end=60 ,dtype='bfloat16').reshape_((3,4,5))
-    t.print()
-    t.save(dir+'bf16')
-
-    t2=load(dir+"bf16")
-    t2.print()
-
 
 if __name__ == "__main__":
-    saveloadfloat32()
-    # saveloadint8()
-    # saveloadbfloat16()
+    saveload("bfloat16")
