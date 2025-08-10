2c8a7208a078349f658471cd2406600c4b1ac967
John <1229775764@qq.com>
2025-08-10 14:36:50 +0800

    decorator

--- front/py/deepx/nn/functional/rtf_matmul.py
+++ front/py/deepx/nn/functional/rtf_matmul.py
@@ -2,6 +2,7 @@ from deepx.tensor import Tensor
 from deepx.nn import DeepxIR,Param
 from deepx.scheduler import send
 
+
 def rtf_matmul(a:Tensor,b:Tensor,out: Tensor ,author='cublas',bench:int=None):
     args=[Param.tensor(a),Param.tensor(b)]
     returns=[Param.tensor(out)]
@@ -9,4 +10,8 @@ def rtf_matmul(a:Tensor,b:Tensor,out: Tensor ,author='cublas',bench:int=None):
     if bench is not None:
         ir._metadata.openbench(bench)
     send(ir)
+    return out
+
+@deepx_op("matmul")
+def rtf_matmul_2(a:Tensor,b:Tensor,out: Tensor ,author='cublas',bench:int=None):
     return out
